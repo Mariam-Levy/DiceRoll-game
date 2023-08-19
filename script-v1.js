2ea3f217b8fd5aacb8ddee725e9c24f35865e679
Mariam-Levy <mariamlevycode@gmail.com>
2023-08-18 22:01:46 -0300

    Button Functionality Added

---
+++ script-v1.js
@@ -0,0 +1,77 @@
+'use strict'
+
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const current0 = document.querySelector('#current--0');
+const current1 = document.querySelector('#current--1');
+
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
+let activePlayer = 0;
+const scores = [0, 0];
+
+// condiciones de inicio:
+score0.textContent = 0;
+score1.textContent = 0;
+current0.textContent = 0;
+current1.textContent = 0;
+
+diceElement.classList.add('hidden'); // esconde el dado
+
+const switchPlayer = function() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;      // reasignando al jugador activo  (Si activePlayer es 0, se cambia a 1; y si es 1, se cambia a 0.)
+    player0.classList.toggle('player--active');      // esta línea de código se usa para cambiar el estado de la clase 'player--active', Si la clase está presente, se quitará; si no está presente, se agregará.
+    player1.classList.toggle('player--active');   
+};
+
+btnRoll.addEventListener('click', function() {
+    // 1.- Necesito que el dado muestre numeros al azar
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2.- Que se muestre el dado
+    diceElement.classList.remove('hidden'); // remueve la clase hidden para que se pueda ver el dado
+    diceElement.src = `dice-${dice}.png` // de esta manera podemos cargar dinamicamente una de las 6 imagnes del dado
+
+    // 3.- Si el dado muestra el numero 1, cambiar de jugador.
+    if (dice !== 1) { //si el dado es diferente a 1, entonces:
+        // añadir el valor del dado a la puntuacion actual
+        currentScore = currentScore + dice; 
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore; // seleccionamos el elemento de la puntuacion de manera dinamica, en funcion de cual es el jugador activo.
+    } else {
+        // cambia de jugador
+        switchPlayer();
+    }
+});
+
+
+btnHold.addEventListener('click', function() {
+    // Agregar la puntuacion actual al jugador que este activo
+    scores[activePlayer] += currentScore;   // esto seria asi: scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+
+    // Comprobar si la puntuacion del jugador es mayor o igual a 100
+    if (scores[activePlayer] >= 20) {
+        //Termina el juego
+        document.querySelector(`player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`player--${activePlayer}`).classList.remove('player--active');
+    } else {
+        // Cambiar al siguiente jugador
+        switchPlayer();
+    }
+
+
+});
+
+
+
+
